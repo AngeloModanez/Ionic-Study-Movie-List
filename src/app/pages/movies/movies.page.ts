ffd1742327f8609d28e598797bc05c0758c6fff6
Bananadev3 <angelomodanez30@gmail.com>
2024-09-09 23:48:12 -0300

    :lipstick: feat: movie Reorder

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/angular';
 import { Movie } from 'src/app/models/movie';
 import { MovieService } from 'src/app/services/movie.service';
 
@@ -22,5 +23,10 @@ export class MoviesPage implements OnInit {
     this.service.getMovies().subscribe({
       next: movies => this.movies = movies
     });
+  }
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+    ev.detail.complete();
   }
 }
